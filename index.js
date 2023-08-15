02e41f2e2cc11423a878780213a032ad1087bde3
Heru Cahyo <113245217+UntilTomorrow@users.noreply.github.com>
2023-08-15 15:03:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-console.log(req.body)
-   });
+    let attlog = req.body.data;
+    attlog.cloud_id = req.body.cloud_id;
+    
+    console.log(attlog);
+    
+    res.send(JSON.stringify({"status": 200, "response": "Data received successfully."}));
+});
 app.listen(process.env.PORT || 3000)
