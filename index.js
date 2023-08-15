0ba425b735aa692ebd83f15b39cf5927649ac569
Heru Cahyo <113245217+UntilTomorrow@users.noreply.github.com>
2023-08-15 14:58:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
-const mysql = require('mysql');
-const conn = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'mysql',
-  database: 'demodb'
-});
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
 
 app.get('/', (req, res) => {
-res.send('Yo!')
 console.log(req.body)
    });
 app.listen(process.env.PORT || 3000)
