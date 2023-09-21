336ede5cae6978243476e08726aff2295f084256
Heru Cahyo <113245217+UntilTomorrow@users.noreply.github.com>
2023-09-21 19:38:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,8 @@ app.use(express.urlencoded({extended:true}));
 
 app.get('/', (req, res) => {
     let attlog = req.body.data;
-    attlog.cloud_id = req.body.cloud_id
-    let sql = "INSERT INTO log SET ?";
-    let query = pool.query(sql, attlog, (err, results) => {
-            if(err) throw err;
-            res.send(JSON.stringify({"status": 200, "response": results}));
-        });
+    attlog.cloud_id = req.body.cloud_id;
+    attlog.type = req.body.type;
     console.log(attlog);
     
     res.send(JSON.stringify({"status": 200, "response": "successfully."}));
