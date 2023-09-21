ba0dc4bce5af4ff4ea3021fa7c150992c039eff1
Heru Cahyo <113245217+UntilTomorrow@users.noreply.github.com>
2023-09-21 15:53:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,21 +2,14 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended:true}));
 
 app.get('/', (req, res) => {
-    const attlogData = req.body.scan;
-    const cloudId = req.body.cloud_id;
-
-    const [date, time] = attlogData.split(' ');
-
-    console.log('Tanggal:', date);
-    console.log('Waktu:', time);
-
-    res.send(JSON.stringify({ "status": 200, "response": "Data received successfully." }));
+    let attlog = req.body.data;
+    attlog.cloud_id = req.body.cloud_id;
+    
+    console.log(attlog);
+    
+    res.send(JSON.stringify({"status": 200, "response": "Data received successfully."}));
 });
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+app.listen(process.env.PORT || 3000)
