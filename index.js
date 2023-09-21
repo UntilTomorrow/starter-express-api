bb2a7efa0e2c443a45a92138aa1c45b0ccc89a6f
Heru Cahyo <113245217+UntilTomorrow@users.noreply.github.com>
2023-09-21 19:02:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,38 +2,14 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended:true}));
 
 app.get('/', (req, res) => {
-    // Mengambil data dari parameter querystring 'data' dan 'cloud_id'
-    const attlogData = req.query.data;
-    const cloudId = req.query.cloud_id;
-
-    // Parse data JSON dari parameter querystring
-    const data = JSON.parse(attlogData);
-
-    // Pisahkan tanggal dan waktu dari data 'scan'
-    const scanDateTime = data.scan.split(' ');
-    const scanDate = scanDateTime[0];
-    const scanTime = scanDateTime[1];
-
-    // Membuat objek baru dengan data yang telah dipisahkan
-    const attlog = {
-        pin: data.pin,
-        date: scanDate,
-        time: scanTime,
-        verify: data.verify,
-        status_scan: data.status_scan,
-        cloud_id: cloudId,
-    };
-
-    // Cetak data yang telah dipisahkan ke konsol
+    let attlog = req.body.data;
+    attlog.cloud_id = req.body.cloud_id;
+    
     console.log(attlog);
-
-    res.send(JSON.stringify({ "status": 200, "response": "successfully." }));
+    
+    res.send(JSON.stringify({"status": 200, "response": "successfully."}));
 });
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+app.listen(process.env.PORT || 3000)
