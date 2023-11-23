37f0f2f33b12c5a1d79345440362167b11cad01a
Heru Cahyo <113245217+UntilTomorrow@users.noreply.github.com>
2023-11-23 09:24:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const TelegramBot = require('node-telegram-bot-api');
 
-const botToken = 'xxx'; // Bot token
+const botToken = '6398537713:AAGc5PMhhbEmsenmrXm0qN_qVzKw_ttBr4I'; // Bot token
 const bot = new TelegramBot(botToken, { polling: true });
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
