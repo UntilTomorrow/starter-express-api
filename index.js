6f3cf96a403c7b18dc8bce1da39d5351c9765d83
Heru Cahyo <113245217+UntilTomorrow@users.noreply.github.com>
2023-12-01 15:01:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
 const express = require('express');
 const app = express();
-const TelegramBot = require('node-telegram-bot-api');
 
-const botToken = 'bot6398537713:AAGc5PMhhbEmsenmrXm0qN_qVzKw_ttBr4I'; // Bot token
-const bot = new TelegramBot(botToken, { polling: true });
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.get('/', (req, res) => {
-    let attlog = req.body.data;
-    attlog.cloud_id = req.body.cloud_id;
-    attlog.type = req.body.type;
-    console.log(attlog);
-    const chatId = '281794994';
-    const message = JSON.stringify(attlog);
-    bot.sendMessage(chatId, message)
-    .then(() => {
-        console.log('Pesan berhasil dikirim ke bot Telegram');
-    })
-    .catch((error) => {
-        console.error('Gagal mengirim pesan ke bot Telegram:', error);
-    });
-    
+    let getdata = req.body.data;
+    getdata.cloud_id = req.body.cloud_id;
+    getdata.type = req.body.type;
+    console.log(getdata);
     res.send(JSON.stringify({"status": 200, "response": "successfully."}));
 });
 app.listen(process.env.PORT || 3000)
