9485d8d286e96fda30f5be547a3d7c542f66b63a
Heru Cahyo <113245217+UntilTomorrow@users.noreply.github.com>
2024-02-01 13:48:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.get('/', (req, res) => {
-    let getdata = req.visitor.name;
+    let getdata = req.body.visitor;
     console.log(getdata);
     res.send(JSON.stringify({"status": 200, "response": "successfully."}));
 });
