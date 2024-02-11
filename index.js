9cb4f66e2362528ba5c6c4fcaa5dac5f68d35af4
Heru Cahyo <113245217+UntilTomorrow@users.noreply.github.com>
2024-02-11 14:23:34 +0700

    update

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const geoip = require("geoip-lite");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.post('/', (req, res) => {
-    const ipAddress = req.ip;
-    // Lakukan sesuatu dengan alamat IP, misalnya menentukan lokasinya
-    // Misalnya, Anda bisa menggunakan layanan geolokasi pihak ketiga untuk menentukan lokasi dari alamat IP
+app.post("/", (req, res) => {
+  const ipAddress = req.ip;
+  const geo = geoip.lookup(ipAddress);
 
-    res.send(JSON.stringify({"status": 200, "response": "successfully.", "ip": ipAddress}));
+  if (geo && geo.ll) {
+    const latitude = geo.ll[0];
+    const longitude = geo.ll[1];
+    res.send(
+      JSON.stringify({
+        status: 200,
+        response: "successfully.",
+        latitude: latitude,
+        longitude: longitude,
+      })
+    );
+  } else {
+    res.send(
+      JSON.stringify({ status: 400, response: "Failed to retrieve location." })
+    );
+  }
 });
 
 app.listen(process.env.PORT || 3000);
