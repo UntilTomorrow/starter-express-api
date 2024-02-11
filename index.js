634e582e51e26b28396897fbcb1eeb37b436207e
Heru Cahyo <113245217+UntilTomorrow@users.noreply.github.com>
2024-02-11 14:07:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
+const geoip = require('geoip-lite');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.post('/', (req, res) => {
-    let getdata = req.body.visitor;
-    console.log(getdata);
-    res.send(JSON.stringify({"status": 200, "response": "successfully."}));
+    const ipAddress = req.ip;
+    const geo = geoip.lookup(ipAddress);
+
+    if (geo && geo.ll) {
+        const latitude = geo.ll[0];
+        const longitude = geo.ll[1];
+        res.send(JSON.stringify({"status": 200, "response": "successfully.", "latitude": latitude, "longitude": longitude}));
+    } else {
+        res.send(JSON.stringify({"status": 400, "response": "Failed to retrieve location."}));
+    }
 });
-app.listen(process.env.PORT || 3000)
+
+app.listen(process.env.PORT || 3000);
